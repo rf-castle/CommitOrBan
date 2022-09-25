a0a109dd1df8be58d294f4fc7dbc35453b5039b2
rf-castle <kouta.tashiro.2000@gmail.com>
2022-09-25 11:42:08 +0900

    api修正

--- pages/api/twitter.ts
+++ pages/api/twitter.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {postImageToTwitter} from "../../lib/twitter";
+import {pickImage} from '../../lib/image';
 
 type Data = {
   // name: string
@@ -13,12 +14,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   }
   const token = req.headers["token"];
   const secret = req.headers["token_secret"]
-  if (typeof token !== "string" || typeof secret !== "string"){
+  const image = req.body;
+  if (typeof token !== "string" || typeof secret !== "string" || typeof image !== "string"){
     res.status(400).send("");
     return;
   }
   try{
-    await postImageToTwitter(token, secret,"")
+    await postImageToTwitter(token, secret, image)
   }
   catch (e){
     console.error(e)
