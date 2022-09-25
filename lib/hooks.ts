8fde647ee426202194d4c220f357f82342c65582
yuporon <ftb.y.m.kashima.07@gmail.com>
2022-09-25 12:01:18 +0900

    Fix: fooksの命名変更

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,19 +1,16 @@
 import { ChangeEventHandler } from 'react'
 import { addPhoto, DarkHistoryPhoto } from './dark_history_photo'
 
-export const useHooks = function (): { handleFiles: ChangeEventHandler<HTMLInputElement> } {
+export const useFileStore = function (): { handleFiles: ChangeEventHandler<HTMLInputElement> } {
   const handleFiles: ChangeEventHandler<HTMLInputElement> = (event) => {
     const func = async (): Promise<void> => {
       const files = event.currentTarget.files
       if (files == null || files?.length === 0) return
       const file = files[0]
-      console.log('file:', file)
-      const contentType = file.name
       const arrayBuffer = await file.arrayBuffer()
       const base64String = btoa(
         String.fromCharCode.apply(null, Array.from<number>(new Uint8Array(arrayBuffer))),
       )
-      console.log(`data:${contentType};base64,${base64String}`)
       const newPhoto: DarkHistoryPhoto = {
         binary: base64String,
         id: new Date().getTime(),
